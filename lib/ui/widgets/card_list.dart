66dd1b83f237d220f864496401de3b8e0e56f90f
HuangmingyuanUestc <49358630+HuangmingyuanUestc@users.noreply.github.com>
2019-10-28 18:23:33 +0800

    main.dart
    This file is the leading-end architecture code. Due to the limited number of files uploaded, only the lib folder was uploaded. This version contains the main function and notes the basic code specification.

---
+++ lib/ui/widgets/card_list.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/**
+ * @auther list[Form:AutherName Date]:
+ * FROM>>
+ *              <<TO HERE
+ *
+ * @recent change list[Form:AutherName  Changed content]:
+ * FROM>>
+ *
+ *              <<TO HERE
+ */
+
+
